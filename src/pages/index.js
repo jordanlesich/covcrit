e5162732f77613e5e4ad9d9ec05cad8bb1b950bd
Jordan Lesich <jordan.lesich@gmail.com>
2020-07-15 19:52:32 -0400

    erased extra tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 import Warning from "../components/warning";
 import ListItem from "../components/listItem";
-import { trimObjFields, getPalette } from "../helpers";
+import { trimObjFields } from "../helpers";
 import styled from "styled-components";
 
 const Listing = styled.ul`
